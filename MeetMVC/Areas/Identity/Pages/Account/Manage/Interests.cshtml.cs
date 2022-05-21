c00e5653f366c6a3384b1362a29095d08ce8a5f6
Rayse <raysedev@protonmail.com>
2022-05-21 23:25:30 +0300

    interests bug fixes

--- MeetMVC/Areas/Identity/Pages/Account/Manage/Interests.cshtml.cs
+++ MeetMVC/Areas/Identity/Pages/Account/Manage/Interests.cshtml.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Data.Entity;
 using MeetMVC.Data;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace MeetMVC.Areas.Identity.Pages.Account.Manage
 {
@@ -12,42 +14,84 @@ namespace MeetMVC.Areas.Identity.Pages.Account.Manage
         public string Message { get; set; }
         public List<SelectListItem> Interests { get; set; }
         private ApplicationDbContext Context { get; }
+        //todo readonly
+        private static readonly string[] interestsLst = { "sports", "gamings", "cooking" };
 
-        public InterestsModel(ApplicationDbContext _context)
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public InterestsModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ApplicationDbContext _context)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
             this.Context = _context;
         }
+        public string Image { get; set; }
+        public string Username { get; set; }
 
-        public void OnGet()
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
         {
-            var interests = (from interest in this.Context.Interests
-                          select new SelectListItem
-                          {
-                              Text = interest.Name,
-                              Value = interest.Id.ToString()
-                          }).ToList();
-            this.Interests = interests;
+            public List<string> UserInterests { get; set; }
         }
 
-        public void OnPostSubmit()
+        private List<SelectListItem> GetInterests()
         {
-            var interests = (from interest in this.Context.Interests
+            return (from interest in interestsLst
                              select new SelectListItem
                              {
-                                 Text = interest.Name,
-                                 Value = interest.Id.ToString()
+                                 Text = interest,
+                                 Value = interest
                              }).ToList();
-            this.Interests = interests;
+            //this.Interests = interests;
+        }
 
-            string[] interestIds = Request.Form["lstInterests"].ToString().Split(",");
-            foreach (string id in interestIds)
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            this.Interests = GetInterests();
+
+            List<string> interestNames = new List<string>();
+            foreach (var interest in user.Interests)
             {
-                if (!string.IsNullOrEmpty(id))
-                {
-                    string name = this.Interests.Where(x => x.Value == id).FirstOrDefault().Text;
-                    this.Message += "Id: " + id + "  Interest: " + name + "\\n";
-                }
+                interestNames.Add(interest.Name);
             }
+
+            Input = new InputModel
+            {
+                UserInterests = interestNames
+            };
+
+            this.Message = interestNames.Count().ToString() + " + " + user.Interests.Count();
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostSubmitAsync()
+        {
+
+            this.Interests = GetInterests();
+            var user = await _userManager.GetUserAsync(User);
+
+            string[] interestNames = Request.Form["lstInterests"].ToString().Split(",");
+            foreach (string name in interestNames)
+            {
+
+                user.Interests.Add(new Interest
+                {
+                    Name = name
+                });
+    
+                this.Message += "Interest: " + name + "\\n";
+            }
+
+            await _userManager.UpdateAsync(user);
+            return RedirectToPage();
         }
     }
 }
--- MeetMVC/Data/Interest.cs
+++ MeetMVC/Data/Interest.cs
@@ -1,8 +1,11 @@
-﻿namespace MeetMVC.Data
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MeetMVC.Data
 {
     public class Interest
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string ApplicationUserId { get; set; }
     }
 }
